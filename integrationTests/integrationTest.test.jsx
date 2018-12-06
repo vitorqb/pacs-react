034edc5a98e83b028916d28bbe1a161eebc6bade
vitor quintanilha barbosa <vitorqb@gmail.com>
2018-12-06 19:00:43 +0100

    Tests run!

--- integrationTests/integrationTest.test.jsx
+++ integrationTests/integrationTest.test.jsx
@@ -1,8 +1,15 @@
 // Integration tests for pacs-react
+import React from 'react';
+import { mount } from 'enzyme';
+import App from '../src/App';
 
 
-describe('Tests run fine', () => {
-  it('This should suceed', () => {
-    expect(true).toBe(true)
+describe('User first interaction', () => {
+  it('List of last Transactions is rendered', () => {
+    // The app is rendered
+    const app = mount(<App />)
+    const transactionList = app.find('transactionList')
+    expect(transactionList).toHaveLength(1)
+    fail("Finish tests!")
   })
 })
