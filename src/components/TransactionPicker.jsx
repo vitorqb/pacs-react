aa2d386a1f36efd83f639edbe17d899335f51f99
vitor quintanilha barbosa <vitorqb@gmail.com>
2021-12-24 22:09:52 +0100

    NEW Adds error callback for TransactionPicker

--- src/components/TransactionPicker.jsx
+++ src/components/TransactionPicker.jsx
@@ -10,6 +10,8 @@ export default class TransactionPicker extends Component {
  * @param {fn(number): Promise<Transaction>} props.getTransaction - A function that
  *   maps a pk to a promise with a Transaction.
  * @param props.onPicked - A callback function called with the picked transaction.
+ * @param props.onGetTransactionFailure - A callback function for when
+ *   getting a transaction fails.
  */
   constructor(props) {
     super(props);
@@ -20,13 +22,20 @@ export default class TransactionPicker extends Component {
     this.setState({pk: e.target.value});
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
+    let transaction;
     const { pk } = this.state;
-    const handleGottenTransaction = (transaction) => {
-      this.props.onPicked(transaction);
-    };    
+    const { getTransaction, onGetTransactionFailure, onPicked } = this.props;
     e.preventDefault();
-    return this.props.getTransaction(pk).then(handleGottenTransaction);
+    try {
+      transaction = await getTransaction(pk);
+    } catch(e) {
+      if (onGetTransactionFailure) {
+        onGetTransactionFailure(e.message);
+      }
+      return;
+    }
+    onPicked(transaction);
   }
 
   render() {
--- src/components/__tests__/TransactionPicker.spec.jsx
+++ src/components/__tests__/TransactionPicker.spec.jsx
@@ -3,6 +3,7 @@ import {mount} from 'enzyme';
 import TransactionPicker from '../TransactionPicker.jsx';
 import sinon from 'sinon';
 import { TransactionFactory } from '../../testUtils';
+import { waitFor } from '../../testUtils.jsx';
 
 describe('TransactionPicker', () => {
 
@@ -22,35 +23,55 @@ describe('TransactionPicker', () => {
 
   describe('Submiting...', () => {
 
-    let picker, onPicked, transaction, transactionPromise, getTransaction;
+    let onPicked, onGetTransactionFailure, transaction, transactionPromise, getTransaction;
 
     beforeEach(() => {
       transaction = TransactionFactory.build();
       transactionPromise = Promise.resolve(transaction);
       onPicked = sinon.fake();
+      onGetTransactionFailure = sinon.fake();
       getTransaction = sinon.fake.returns(transactionPromise);
-      picker = mount(
-        <TransactionPicker getTransaction={getTransaction} onPicked={onPicked} />
-      );;
     });
 
+    const render = () => mount(
+      <TransactionPicker
+        getTransaction={getTransaction}
+        onPicked={onPicked}
+        onGetTransactionFailure={onGetTransactionFailure}
+      />
+    );
+
     it('Calls getTransaction with pk on submit', () => {
-      picker
-        .find('input[name="pk"]')
-        .simulate('change', {target: {value: 12}});
+      const picker = render();
+      picker.find('input[name="pk"]').simulate('change', {target: {value: 12}});
       picker.find("form").simulate("submit");
       picker.update();
       expect(getTransaction.calledWith(12)).toBe(true);
     });
+
     it('Calls callback onPicked with gotten transaction on submit', async () => {
-      picker
-        .find('input[type="submit"]')
-        .simulate('change', {target: {value: 12}});
+      const picker = render();
+      picker.find('input[type="submit"]').simulate('change', {target: {value: 12}});
       picker.update();
       picker.instance().forceUpdate();
       picker.find("form").simulate("submit");
       await transactionPromise.then().then();
       expect(onPicked.calledWith(transaction)).toBe(true);
+    });
+
+    it('Calls callback onGetTransactionFailure with error if fails to get the transaction', async () => {
+      const error = "Not found!";
+      getTransaction = sinon.fake.rejects(error);
+      const picker = render();
+      picker.find('input[type="submit"]').simulate('change', {target: {value: 12}});
+      picker.update();
+      picker.instance().forceUpdate();
+      picker.find("form").simulate("submit");
+      await transactionPromise.then().then();
+      await waitFor(() => onGetTransactionFailure.args.length > 0);
+      expect(onPicked.args).toHaveLength(0);
+      expect(onGetTransactionFailure.args.length).toEqual(1);
+      expect(onGetTransactionFailure.args[0][0]).toEqual(error);
     });
   });
 });
