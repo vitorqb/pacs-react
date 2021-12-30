0c7db013df0635c0564e4c1ae6e0876a22d3dc13
vitor quintanilha barbosa <vitorqb@gmail.com>
2021-12-30 18:43:34 +0100

    NEW Adds ShortcutService

---
+++ src/services/ShortcutService.js
@@ -0,0 +1,41 @@
+import * as R from 'ramda';
+
+
+export class ShortcutService {
+
+  /**
+   * Constructor
+   * @param {object} opts
+   * @param {object} opts.aDocument The `window.document` to bind to.
+   * @param {function} opts.hotkeys The `hotkeys` fn from hotkeys-js package.
+   */
+  constructor({aDocument, hotkeys}) {
+    this._document = aDocument;
+    this._hotkeys = hotkeys;
+    this._shortcuts = [];
+  }
+
+  /**
+   * Initializes the service by listening to the shortcuts.
+   */
+  async init() {
+    const promises = this._shortcuts.map(([shortcut, handler]) => {
+      return this._hotkeys(shortcut, {element: this._document}, () => handler());
+    });
+    return await Promise.all(promises);
+    ;
+  }
+
+  /**
+   * Register a new shortcut.
+   * @param {string} shortcut The shortcut (string).
+   * @param {Function} handler The 0-arg handler.
+   */
+  register(shortcut, handler) {
+    this._shortcuts = R.append([shortcut, handler])(this._shortcuts);
+  }
+  
+
+}
+
+
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
+
 require("../node_modules/jest-enzyme/lib/index.js");
 configure({ adapter: new Adapter() });
